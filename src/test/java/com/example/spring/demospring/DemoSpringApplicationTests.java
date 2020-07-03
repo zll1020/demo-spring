4d6add8cc7d84017d65ce273dfecf509b4367f38
zhangll6 <zhangll6@ziroom.com>
2020-07-03 11:19:10 +0800

    Create

---
+++ src/test/java/com/example/spring/demospring/DemoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.demospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
